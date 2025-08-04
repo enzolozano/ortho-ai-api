3cc37e0474c58e1803738a4a2e0c167bf9b51ae4
Enzo <enzo.malozano@gmail.com>
2025-08-04 10:57:35 -0300

    Update router

--- ortho-ai-api/app/api/v1/endpoints/users.py
+++ ortho-ai-api/app/api/v1/endpoints/users.py
@@ -8,7 +8,7 @@ from data.database import get_db
 router = APIRouter()
 
 @router.post(
-    "/users", 
+    "/", 
     response_model=user_schema.UserResponse,
     summary="Criar um novo usuário",
     description="Cria um novo usuário no sistema com os dados fonecidos."
@@ -17,7 +17,7 @@ async def create_user(user: user_schema.UserCreate, db: Session = Depends(get_db
     return user_service.create_user(db=db, user=user)
 
 @router.get(
-    "/users", 
+    "/", 
     response_model=List[user_schema.UserResponse],
     summary="Listar os usuários do sistema",
     description="Retorna uma lista de todos os usuários cadastrados no sistema."
@@ -26,7 +26,7 @@ async def list_users(db: Session = Depends(get_db)):
     return user_service.get_users(db)
 
 @router.get(
-    "/users/{user_id}", 
+    "/{user_id}", 
     response_model=user_schema.UserResponse,
     summary="Pesquisar um usuário pelo id",
     description="Retorna um usuário com o id igual ao fornecido."
@@ -35,7 +35,7 @@ async def get_user(user_id: int, db: Session = Depends(get_db)):
     return user_service.get_user(db, user_id=user_id)
 
 @router.get(
-    "/users/by_role/{role}",
+    "/by_role/{role}",
     response_model=List[user_schema.UserResponse],
     summary="Listar os usuários pelo tipo",
     description="Retorna uma lista de usuários a partir do tipo (Paciente, Médico ou Administrador)"
@@ -44,7 +44,7 @@ async def get_users_by_role(role: int, db: Session = Depends(get_db)):
     return user_service.get_users_by_role(db, role=role)
 
 @router.delete(
-    "/users/{user_id}",
+    "/{user_id}",
     summary="Excluir um usuário pelo id",
     description="Exclui um usuário no sistema a partir do id do mesmo."
 )
--- ortho-ai-api/app/enumerators/user_role.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class UserRole(Enum):
-    PATIENT = 0
-    MEDIC = 1
-    ADMINISTRATOR = 2
